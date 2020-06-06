e7c9fcf4f81ba108bf470964fdf5b1926287e797
Luke Cheng <chenglu@users.noreply.github.com>
2020-06-06 17:28:30 +0800

    Update deploy-cn.sh

--- tool/translator/deploy-cn.sh
+++ tool/translator/deploy-cn.sh
@@ -11,8 +11,6 @@ rm -rf /tmp/site-dart.cn/ || true
 git clone https://chenglu:${CHENGLU_DEPLOY_KEY}@github.com/chenglu/site-dart.cn.git /tmp/site-dart.cn/
 
 # cd /tmp/site-dart.cn/
-# git config --global user.name "travis-ci deploy"
-# git config --global user.email "cfug-dev@googlegroups.com"
 
 # git rm -rf .
 # git add .
@@ -21,6 +19,9 @@ git clone https://chenglu:${CHENGLU_DEPLOY_KEY}@github.com/chenglu/site-dart.cn.
 cp -r _site/* /tmp/site-dart.cn/
 
 cd /tmp/site-dart.cn/
+
+git config --global user.name "travis-ci deploy"
+git config --global user.email "cfug-dev@googlegroups.com"
 
 git add .
 git commit --allow-empty -am "${commitMessage}"
