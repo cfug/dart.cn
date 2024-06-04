b878e2fee39b051f2346558847903bb5ceaa8e9f
Amos <amoshuke@qq.com>
2024-06-04 17:11:36 +0800

    Update: tool/host-redirect/build.sh

--- tool/host-redirect/build.sh
+++ tool/host-redirect/build.sh
@@ -12,5 +12,5 @@ git config --global user.name "cfug-dev"
 git config --global user.email "cfug-dev@googlegroups.com"
 
 git add ./src/content/assets/js/host-redirect.min.js
-git commit -m "[sync] Update: host-redirect.min.js"
+git commit -m "[🤖bot] Update: host-redirect.min.js"
 git push -u -f
